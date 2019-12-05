a0b739abe88ee388f1b99bf0e8f4c90f4ef8acd0
tlvprasad <lvprasad.t@gmail.com>
2019-12-05 13:52:58 +0530

    Initial commit

---
+++ customlib/src/main/java/com/lvprasad/customlib/Point.java
@@ -0,0 +1,19 @@
+package com.lvprasad.customlib;
+
+public class Point {
+    float mLatitude;
+    float mLongitude;
+
+    public Point(float latitude, float longitude) {
+        mLatitude = latitude;
+        mLongitude = longitude;
+    }
+
+    public float getLatitude() {
+        return mLatitude;
+    }
+
+    public float getLongitude() {
+        return mLongitude;
+    }
+}
